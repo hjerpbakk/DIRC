ac42c92d82895dd670fa1595d868a6a762308750
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-01-28 17:04:36 +0100

    Added INotifyPropertyChanged

--- DIRC/ViewModels/ViewModel.cs
+++ DIRC/ViewModels/ViewModel.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DIRC.Annotations;
 
 namespace DIRC {
-	public class ViewModel {
+	public class ViewModel : INotifyPropertyChanged {
 		public ViewModel() {
 		}
+
+	    public event PropertyChangedEventHandler PropertyChanged;
+
+	    [NotifyPropertyChangedInvocator]
+	    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	    {
+	        var handler = PropertyChanged;
+	        if (handler != null)
+	        {
+	            handler(this, new PropertyChangedEventArgs(propertyName));
+	        }
+	    }
 	}
 }
 
