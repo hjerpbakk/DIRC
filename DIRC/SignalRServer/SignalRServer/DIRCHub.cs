b84b2b22c02766b260d9305ae0fda7748cb6d8e6
Øyvin Richardsen <ori@dips.no>
2015-01-28 19:29:14 +0100

    server init

---
+++ DIRC/SignalRServer/SignalRServer/DIRCHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRServer
+{
+    public class DIRCHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
