34e13f2292eff2f9b4a58646a2fd11db664ec8ed
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-01-28 17:28:14 +0100

    asdas

---
+++ DIRC/View/LogonView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DIRC.View {
+	public partial class LogonView : ContentPage {
+		public LogonView() {
+			InitializeComponent();
+			BindingContext = new LogonViewModel(Navigation);
+			NavigationPage.SetHasNavigationBar(this, false);
+		}
+	}
+}
+
---
+++ DIRC/View/MessagesView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DIRC.View {
+	public partial class MessagesView : ContentPage {
+		public MessagesView() {
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+		}
+	}
+}
+
