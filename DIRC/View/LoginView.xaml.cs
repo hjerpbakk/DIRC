ff52a3249cddd613f142313b6560bd1752fda5b9
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-01-28 16:57:57 +0100

    Running application

---
+++ DIRC/View/LoginView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DIRC.View {
+	public partial class LoginView : ContentPage {
+		public LoginView() {
+			InitializeComponent();
+		}
+	}
+}
+
