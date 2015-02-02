1ea6e1b4416a69561fc969e70eadd60ee9b09074
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-02-02 20:51:44 +0100

    Serverupdates

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM mono:latest
 ADD WebSocketSpike WebSocketSpike
 RUN xbuild /WebSocketSpike/WebSocketSpike.sln
 
-EXPOSE 8080
+EXPOSE 1337
 CMD mono /WebSocketSpike/WebSocketSpike.LocalWebServer/bin/Debug/WebSocketSpike.LocalWebServer.exe
--- WebSocketSpike/WebSocketSpike.LocalWebServer/DircHub.cs
+++ WebSocketSpike/WebSocketSpike.LocalWebServer/DircHub.cs
@@ -6,34 +6,32 @@ namespace WebSocketSpike.LocalWebServer
 {
     public class DircHub : Hub
     {
-        public void Send(string message)
+        public void Send(string user, string platform, string message)
         {
             Console.WriteLine("Broadcast message \"{0}\" from {1} to others.", message, Context.ConnectionId);
-            Clients.Others.broadcastMessage(message);
-        }
-
-        public void SendDIRCMessage(string message)
-        {
-            Console.WriteLine("DIRC message \"{0}\" from {1} to others.", message, Context.ConnectionId);
-            Clients.Others.broadcastDIRCMessage(message);
+            Clients.Others.broadcastMessage(user, platform, message);
+            Clients.Others.broadcastMessage(Context.ConnectionId, user, platform, message);
         }
 
         public override Task OnConnected()
         {
             Console.WriteLine("Connection from {0}", Context.ConnectionId);
-            Clients.Caller.broadcastMessage("Welcome. Your connectionId is " + Context.ConnectionId);
+            Clients.Caller.broadcastMessage("DircHub", "GNU/Linux", "Welcome. Your connectionId is " + Context.ConnectionId);
+            Clients.Others.broadcastConnect(Context.ConnectionId);
             return base.OnConnected();
         }
 
         public override Task OnDisconnected(bool stopCalled)
         {
             Console.WriteLine("OnDisconnected {0}, from {1}", stopCalled, Context.ConnectionId);
+            Clients.Others.broadcastDisconnect(Context.ConnectionId);
             return base.OnDisconnected(stopCalled);
         }
 
         public override Task OnReconnected()
         {
             Console.WriteLine("OnReconnected from {0}", Context.ConnectionId);
+            Clients.Others.broadcastReconnect(Context.ConnectionId);
             return base.OnReconnected();
         }
     }
