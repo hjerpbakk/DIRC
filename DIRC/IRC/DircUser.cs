4a00c06de798e8f9a3f5a23ababe3d4d72a70603
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-02-04 21:56:06 +0100

    Emjoiii #18

--- DIRC/IRC/DircUser.cs
+++ DIRC/IRC/DircUser.cs
@@ -16,13 +16,11 @@ namespace DIRC.IRC {
 						case TargetPlatform.iOS:
 							return "";
 						case TargetPlatform.Android:
-							return "";
+							return "\ud83d\udc7d";
 						case TargetPlatform.WinPhone:
-							return "U+1F38F";
+							return "\ud83d\udeaa";
 						default:
 							switch (platform.ToLower()) {
-								case "safari":
-									return "";
 								case "gnu/linux":
 									return "\ud83d\udcbb";
 							}
@@ -41,5 +39,17 @@ namespace DIRC.IRC {
 			return "(" + platform + "):";
 		}
 	}
+
+//	platform_icons["default"] = '<i class="fa fa-question"></i>';
+//	platform_icons["android"] = '<i class="fa fa-android"></i>';
+//	platform_icons["ios"] = '<i class="fa fa-apple"></i>';
+//	platform_icons["winphone"] = '<i class="fa fa-windows"></i>';
+//	platform_icons["windows"] = '<i class="fa fa-windows"></i>';
+//	platform_icons["gnu/linux"] = '<i class="fa fa-linux"></i>';
+//	platform_icons["safari"] = '<i class="fa fa-compass"></i>';
+//	platform_icons["chrome"] = '<i class="icon-chrome"></i>';
+//	platform_icons["opera"] = '<i class="icon-opera"></i>';
+//	platform_icons["ie"] = '<i class="icon-ie"></i>';
+//	platform_icons["firefox"] = '<i class="icon-firefox"></i>';
 }
 
