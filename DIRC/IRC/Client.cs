4266bb60fcd17f0ddfe2799cf179c05bd6b01f9d
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-02-05 09:20:15 +0100

    Updated client

--- DIRC/IRC/Client.cs
+++ DIRC/IRC/Client.cs
@@ -7,84 +7,90 @@ using System.Linq;
 
 namespace DIRC.IRC
 {
-	public class Client
-	{
-		readonly string userName;
-		readonly string platform;
-		readonly HubConnection connection;
-		readonly IHubProxy hub;
+    public class Client
+    {
+        readonly string userName;
+        readonly string platform;
+        readonly HubConnection connection;
+        readonly IHubProxy hub;
 
-		public event EventHandler<string> OnMessageReceived;
-		public event EventHandler<IList<DircUser>> OnConnectedToHub;
-		public event EventHandler<DircUser> OnNewUser;
-		public event EventHandler<string> OnUserLeft;
+        public event EventHandler<string> OnMessageReceived;
+        public event EventHandler<IList<DircUser>> OnConnectedToHub;
+        public event EventHandler<DircUser> OnNewUser;
+        public event EventHandler<string> OnUserLeft;
 
-		public Client(string userName)
-		{
-			this.userName = userName;
-			platform = Device.OS.ToString();
-			connection = new HubConnection("http://mildestve.it:1337");
-			hub = connection.CreateHubProxy("DircHub");
-			hub.On("broadcastMessage", (string theUserName, string thePlatform, string message) =>
-				{
-					var messageReceived = OnMessageReceived;
-					if (messageReceived != null) {
-						messageReceived(this, string.Format("{0} {1} {2}", theUserName, DircUser.GetPlatformText(thePlatform), message));
-					}
-				});
+        public Client(string userName)
+        {
+            this.userName = userName;
+            platform = Device.OS.ToString();
+            connection = new HubConnection("http://mildestve.it:1337");
+            hub = connection.CreateHubProxy("DircHub");
+            hub.On("broadcastMessage", (string theUserName, string thePlatform, string message) =>
+            {
+                var messageReceived = OnMessageReceived;
+                if (messageReceived != null)
+                {
+                    messageReceived(this, string.Format("{0} ({1}): {2}", theUserName, thePlatform, message));
+                }
+            });
 
-			hub.On("broadcastActiveUsers", (IEnumerable<object> users) =>
-				{
-					var connectedToHub = OnConnectedToHub;
-					if (connectedToHub != null) {
-						connectedToHub(this, users.Cast<DircUser>().ToList());
-					}
-				});
+            hub.On("broadcastActiveUsers", (IEnumerable<object> users) =>
+            {
+                var connectedToHub = OnConnectedToHub;
+                if (connectedToHub != null)
+                {
+                    connectedToHub(this, users.Cast<DircUser>().ToList());
+                }
+            });
 
-			hub.On("broadcastNewUser", (string connectionId, string theUserName, string thePlatform) =>
-				{
-					var onNewUser = OnNewUser;
-					if (onNewUser != null) {
-						onNewUser(this, new DircUser { ConnectionId = connectionId, UserName = theUserName, Platform = thePlatform });
-					}
-				});
+            hub.On("broadcastNewUser", (string connectionId, string theUserName, string thePlatform) =>
+            {
+                var onNewUser = OnNewUser;
+                if (onNewUser != null)
+                {
+                    onNewUser(this, new DircUser { ConnectionId = connectionId, UserName = theUserName, Platform = thePlatform });
+                }
+            });
 
-			hub.On("broadcastUserLeft", (string connectionId) =>
-				{
-					var onUserLeft = OnUserLeft;
-					if (onUserLeft != null) {
-						onUserLeft(this, connectionId);
-					}
-				});
-		}
+            hub.On("broadcastUserLeft", (string connectionId) =>
+            {
+                var onUserLeft = OnUserLeft;
+                if (onUserLeft != null)
+                {
+                    onUserLeft(this, connectionId);
+                }
+            });
+        }
 
-		public async Task Connect()
-		{
-			await connection.Start();
-			// TODO: Funker sikkert med ny hub..?
-			//await hub.Invoke("register", userName, platform);
-		}
+        public async Task Connect()
+        {
+            await connection.Start();
+            await hub.Invoke("register", userName, platform);
+        }
 
-		public async Task Send(string message)
-		{
-			bool failed = false;
-			try {
-				await SendMessage(message);
-			} catch (Exception)
-			{
-				failed = true;
-			}
+        public async Task Send(string message)
+        {
+            bool failed = false;
+            try
+            {
+                await SendMessage(message);
+            }
+            catch (Exception)
+            {
+                failed = true;
+            }
 
-			if (failed)
-			{
-				await Connect();
-				await SendMessage(message);
-			}
-		}
+            if (failed)
+            {
+                await Connect();
+                await SendMessage(message);
+            }
+        }
 
-		async Task SendMessage(string message) {
-			await hub.Invoke("send", userName, platform, message);
-		}
-	}
+        async Task SendMessage(string message)
+        {
+            await hub.Invoke("send", userName, platform, message);
+        }
+    }
 }
 
